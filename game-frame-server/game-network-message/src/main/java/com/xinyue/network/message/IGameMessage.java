948cce104e15b42e36c50bda641f87af14b4b1ee
wgslucky <291123097@qq.com>
2018-06-03 23:34:00 +0800

    添加协议头接口

--- game-frame-server/game-network-message/src/main/java/com/xinyue/network/message/IGameMessage.java
+++ game-frame-server/game-network-message/src/main/java/com/xinyue/network/message/IGameMessage.java
@@ -3,4 +3,6 @@ package com.xinyue.network.message;
 public interface IGameMessage {
 
 	MessageHead getMessageHead();
+	
+	
 }
--- game-frame-server/game-network-message/src/main/java/com/xinyue/network/message/MessageHead.java
+++ game-frame-server/game-network-message/src/main/java/com/xinyue/network/message/MessageHead.java
@@ -3,9 +3,48 @@ package com.xinyue.network.message;
 public class MessageHead {
 	// 在与客户端传输中需要序列化的字段
 	private int messageId;
+	// 消息序列号
+	private int seqId;
+	// 消息版本
+	private short msgVersion;
 
+	// 错误码，只有服务器返回给客户端的消息中才会有。
+	private int errorCode;
+	private String errorMsg;
 	// 服务器内部使用的字段，不需要在传输中序列化的字段
 	private long userId;
+
+	public int getSeqId() {
+		return seqId;
+	}
+
+	public void setSeqId(int seqId) {
+		this.seqId = seqId;
+	}
+
+	public short getMsgVersion() {
+		return msgVersion;
+	}
+
+	public void setMsgVersion(short msgVersion) {
+		this.msgVersion = msgVersion;
+	}
+
+	public int getErrorCode() {
+		return errorCode;
+	}
+
+	public void setErrorCode(int errorCode) {
+		this.errorCode = errorCode;
+	}
+
+	public String getErrorMsg() {
+		return errorMsg;
+	}
+
+	public void setErrorMsg(String errorMsg) {
+		this.errorMsg = errorMsg;
+	}
 
 	public int getMessageId() {
 		return messageId;
